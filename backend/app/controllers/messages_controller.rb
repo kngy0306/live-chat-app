906943a7641c4cb307a586c0dd7a1ba54801a728
kona <kitami.kitano20@gmail.com>
2022-10-26 23:38:16 +0900

    :tada: [Add] messages一覧取得API実装

---
+++ backend/app/controllers/messages_controller.rb
@@ -0,0 +1,18 @@
+class MessagesController < ApplicationController
+  def index
+    messages = Message.eager_load(:user)
+    logger.debug("Message.allで取得した結果 #{messages}")
+    messages_array = messages.map do |message|
+      {
+        id: message.id,
+        user_id: message.user.id,
+        name: message.user.name,
+        content: message.content,
+        email: message.user.email,
+        created_at: message.created_at
+      }
+    end
+    logger.debug("Message.all→mapで整形した結果 #{messages_array}")
+    render json: messages_array, status: 200
+  end
+end
