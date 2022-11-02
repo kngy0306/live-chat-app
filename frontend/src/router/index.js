b7752ab43e66f924f69f38d350f63085af4e5dd1
kona <kitami.kitano20@gmail.com>
2022-11-02 23:03:05 +0900

    :construction: [WIP]add welcome page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
